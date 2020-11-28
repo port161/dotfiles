a71e90228c0d81864111e4f981da198d1294ab51
port161 <>
2020-11-28 14:24:51 +0000

    fixed typo

--- custom_scripts/displayStartup.sh
+++ custom_scripts/displayStartup.sh
@@ -2,4 +2,5 @@
 
 displayID=$(xrandr | grep connected | awk -F ' ' '{print $1}')
 
-xrandr --ouptu $displayID --mode 1920x1080 --pos 0x0 --rotate normal; feh --bg-scale ~/Pictures/Wallpapers/wallpaper.jpg
+xrandr --output $displayID --mode 1920x1080 --pos 0x0 --rotate normal; feh --bg-scale ~/Pictures/Wallpapers/wallpaper.jpg
+
